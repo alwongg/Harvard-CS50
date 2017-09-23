e4fc82aefa8c037fa3ac45ce587130a632d67197
Alex <wong_1994@hotmail.com>
2017-09-23 14:42:13 -0400

    Fixed bug and accepted by CS50

--- Pset2/Initials/less/initials.c
+++ Pset2/Initials/less/initials.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 #include <cs50.h>
-#include <string.h>
 #include <ctype.h>
+#include <string.h>
+
+// ctype.h is needed for the toupper function - to capitalize a character in a string
+// string.h is needed for strlen - to find out the length of a string
+// cs50.h is needed for get_stirng() - to get a string from the user
+// stdio.h is needed for printf - to print to the console
 
 int main(void)
 {
-    string name = get_string("Name: ");
+    // Get a string from the user
+    string name = get_string();
+    // Don't need to worry about spaces so just capitalize and print the first character in string
     printf("%c", toupper(name[0]));
+    // Iterate through the given string
     for (int i = 0, n = strlen(name); i < n; i++)
     {
-        if (name[i] == ' ' && name[i+1] != '\0')
+        // If the character in name is a space
+        if (name[i] == ' ')
         {
-            printf("%c", toupper(name[i+1]));
-            i++;
+            // The next character after the space will be capitalized
+            printf("%c", toupper(name[i + 1]));
         }
     }
+    // Print new line
     printf("\n");
 }
